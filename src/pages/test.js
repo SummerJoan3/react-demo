8519d124afd393c488272aae3c60a0a6133977fd
xjq <lebnizjoan@gmail.com>
2020-03-21 00:48:52 +0800

    封装请求

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import styles from './styles.scss';
 import s from './test.scss';
+import { fetch } from '../utils';
 const Test = () => {
+  fetch.post('/page/404', { a: 1 }).then(res => {
+    console.log(res);
+  });
   return (
     <div className={styles.big}>
       adsad
