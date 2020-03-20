a9ab7e34cc0c11d1004e3cff6cbd4c402d8d3fb6
xjq <lebnizjoan@gmail.com>
2020-03-20 19:46:26 +0800

    配置css打包以及loader

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import styles from './styles.scss'
+import s from './test.scss'
 const Test = () => {
-  return <div className={styles.big}>adsad</div>
+  return (
+    <div className={styles.big}>
+      adsad
+      <a href='http://www.w3school.com.cn' className={s.big}>
+        sdasd
+      </a>
+    </div>
+  )
 }
 
 export default Test
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const devMode = process.env.NODE_ENV !== 'production'
 module.exports = {
   entry: './index.js',
   output: {
@@ -15,12 +17,19 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader?modules=true', 'sass-loader']
+        use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader?modules=true', 'sass-loader']
       }
     ]
   },
+  plugins: [
+    new UglifyJsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].css',
+      chunkFilename: devMode ? '[id].css' : '[id].css'
+    })
+  ],
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'dist')
   }
 }
